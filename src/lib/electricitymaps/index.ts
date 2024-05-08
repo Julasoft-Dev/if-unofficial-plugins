4ac184902c040b0f907b9c07b0fe493b5bdf0d6f
R. G. Koch <koch.rg@gmail.com>
2024-05-08 12:58:45 -0300

    Change ElectricityMaps plugin to new interface

--- src/lib/electricitymaps/index.ts
+++ src/lib/electricitymaps/index.ts
@@ -6,79 +6,24 @@ import axios from 'axios';
 import {ERRORS} from '../../util/errors';
 import {buildErrorMessage} from '../../util/helpers';
 
-import {KeyValuePair, PluginParams} from '../../types/common';
 import {PluginInterface} from '../../interfaces';
-
+import {KeyValuePair, PluginParams} from '../../types/common';
 
 const {AuthorizationError, InputValidationError, APIRequestError} = ERRORS;
-
 const BASE_URL = 'https://api.electricitymap.org/v3';
 
-export class ElectricityMaps implements PluginInterface {
 
-    metadata = {kind: 'execute'};
-    authorizationHeader = '';
-    errorBuilder = buildErrorMessage(ElectricityMaps.name);
-    async configure(): Promise<PluginInterface> {
-            this.initAuth();
-            return this;
-    }
-    async execute(inputs: PluginParams[]): Promise<PluginParams[]> {
-            return inputs.map((model_param)=>{
-                let unit = 'gCO2eq/kWh';
-                let power_consumption = model_param.power_consumption;
-                if (!model_param.power_consumption) {
-                    power_consumption = 1;
-                    unit = 'gCO2eq';
-                }
-                const start = dayjs(model_param.timestamp);
-                const end = start.add(model_param.duration, 'second');
-                const carbon_intensities = this.get_carbon_intensity(model_param.longitude, model_param.latitude, start, end);
-                const hours = Math.floor(model_param.duration / 3600);
-                const blocs = [...Array(hours).keys()]
-                // Calculate for each full hour the ratio of the hour that is in the bloc
-                const hourly_ratios = blocs.map((hour) => {
-                    // this is the first hourly bloc, the ratio is the time between the start and the next hour.
-                    if (hour === 0) {
-                        return start.add(1, 'hour').startOf('hour').diff(start, 'second') / 3600;
-                    }
-                    // this is the last hourly bloc, the ratio is the time between the start of the hour and the end.
-                    if (hour === hours - 1) {
-                        return end.diff(start.add(hour, 'hour').startOf('hour'), 'second') / 3600;
-                    }
-                    return 1;
-                });
-                let total_carbon_intensity = 0;
-                carbon_intensities.then((test) =>
-                    test.forEach(
-                    (carbon_intensity, index)=>{
-                        total_carbon_intensity += carbon_intensity.value * hourly_ratios[index];
-                    }
-                ));
-                return {
-                    ...model_param,
-                    carbon_intensity: total_carbon_intensity * power_consumption,
-                    unit: unit
-                }
-            });
-    }
-    initAuth() {
-        dotenv.config();
-        const token = process.env.EMAPS_TOKEN;
-        if (!token) {
-            throw new AuthorizationError(
-                this.errorBuilder({
-                    message: 'Missing token',
-                    scope: 'authorization',
-                })
-            );
-        }
-        this.authorizationHeader = `auth-token: ${token}`;
-    }
-    private async get_carbon_intensity(longitude: number, latitude: number, start: dayjs.Dayjs, end: dayjs.Dayjs): Promise<KeyValuePair[]> {
+export const ElectricityMaps = (): PluginInterface => {
+
+    const metadata = {kind: 'execute'};
+    const errorBuilder = buildErrorMessage(ElectricityMaps.name);
+
+    let authorizationHeader = '';
+
+    const get_carbon_intensity = async (longitude: number, latitude: number, start: dayjs.Dayjs, end: dayjs.Dayjs): Promise<KeyValuePair[]> => {
         if (isNaN(latitude) || isNaN(longitude)) {
             throw InputValidationError(
-                this.errorBuilder({
+                errorBuilder({
                     message: 'Longitude and Latitude are required for the Electricity Maps API.',
                     scope: 'input',
                 })
@@ -92,7 +37,7 @@ export class ElectricityMaps implements PluginInterface {
         }
         if (end.diff(start, 'days') > 10) {
             throw InputValidationError(
-                this.errorBuilder({
+                errorBuilder({
                     message: 'The maximum duration is 10 days for the Electricity Maps API.',
                     scope: 'input',
                 })
@@ -104,14 +49,14 @@ export class ElectricityMaps implements PluginInterface {
             {
                 params: parameters,
                 headers: {
-                    'auth-token': this.authorizationHeader,
+                    'auth-token': authorizationHeader,
                 }
             }
         ).then(
-            (response)=>{
+            (response: any) => {
                 if (response.status !== 200) {
                     throw new APIRequestError(
-                        this.errorBuilder({
+                        errorBuilder({
                             message: `Error: ${JSON.stringify(response.status)}`,
                         })
                     );
@@ -126,4 +71,61 @@ export class ElectricityMaps implements PluginInterface {
             }
         )
     }
+
+    const execute = async (inputs: PluginParams[]) => {
+        dotenv.config();
+        const token = process.env.EMAPS_TOKEN;
+        if (!token) {
+            throw new AuthorizationError(
+                errorBuilder({
+                    message: 'Missing token',
+                    scope: 'authorization',
+                })
+            );
+        }
+
+        authorizationHeader = `auth-token: ${token}`;
+        return inputs.map((model_param)=>{
+            let unit = 'gCO2eq/kWh';
+            let power_consumption = model_param.power_consumption;
+            if (!model_param.power_consumption) {
+                power_consumption = 1;
+                unit = 'gCO2eq';
+            }
+            const start = dayjs(model_param.timestamp);
+            const end = start.add(model_param.duration, 'second');
+            const carbon_intensities = get_carbon_intensity(model_param.longitude, model_param.latitude, start, end);
+            const hours = Math.floor(model_param.duration / 3600);
+            const blocs = [...Array(hours).keys()]
+            // Calculate for each full hour the ratio of the hour that is in the bloc
+            const hourly_ratios = blocs.map((hour) => {
+                // this is the first hourly bloc, the ratio is the time between the start and the next hour.
+                if (hour === 0) {
+                    return start.add(1, 'hour').startOf('hour').diff(start, 'second') / 3600;
+                }
+                // this is the last hourly bloc, the ratio is the time between the start of the hour and the end.
+                if (hour === hours - 1) {
+                    return end.diff(start.add(hour, 'hour').startOf('hour'), 'second') / 3600;
+                }
+                return 1;
+            });
+            let total_carbon_intensity = 0;
+            carbon_intensities.then((test) =>
+                test.forEach(
+                (carbon_intensity:any, index:number)=>{
+                    total_carbon_intensity += carbon_intensity.value * hourly_ratios[index];
+                }
+            ));
+            return {
+                ...model_param,
+                carbon_intensity: total_carbon_intensity * power_consumption,
+                unit: unit
+            }
+        });
+    }
+
+    return {
+        metadata,
+        execute,
+    };
 }
